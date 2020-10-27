7979aeac77fb6b7518db800f02537c051f05d67c
Nathan Ward <nathan.ward@puppet.com>
2020-10-26 22:27:22 -0700

    Add .clang-format and format code

--- midi-tools.h
+++ midi-tools.h
@@ -16,51 +16,36 @@
 
 /** An array of the 12 note names from C through B (using uppercase letters and
  * sharps, e.g. "C#") */
-const std::array<std::string, 12> noteNames = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"};
+const std::array<std::string, 12> noteNames = {
+    "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"};
 
 /** Given a `noteName`, e.g. "C#", return the lowest available MIDI note, or -1
  * if `noteName` is invalid. */
-int getLowestMidiNote(std::string noteName)
-{
-  for (auto i = 0; i <= noteNames.size(); i++)
-  {
-    if (noteNames[i] == noteName)
-      return i;
+int getLowestMidiNote(std::string noteName) {
+  for (auto i = 0; i <= noteNames.size(); i++) {
+    if (noteNames[i] == noteName) return i;
   }
   return -1;
 }
 
 /** Given a `noteName`, e.g. "C#", and an octave, e.g. `4`, return the MIDI note
  * number. Defaults to the middle octave (4). */
-int getMidiNote(std::string noteName = "C", int octave = 4)
-{
+int getMidiNote(std::string noteName = "C", int octave = 4) {
   int note = getLowestMidiNote(noteName) + (octave + 1) * 12;
 
-  if (note < 0)
-  {
-    return 0; // C-1
-  }
-  else if (note > 127)
-  {
-    return 127; // G9
-  }
-  else
-  {
+  if (note < 0) {
+    return 0;  // C-1
+  } else if (note > 127) {
+    return 127;  // G9
+  } else {
     return note;
   }
 }
 
 /** Array of mode names */
 const std::string modeNames[9] = {
-    "ionian",
-    "dorian",
-    "phrygian",
-    "lydian",
-    "mixolydian",
-    "aeolian",
-    "locrian",
-    "harmonic",
-    "melodic",
+    "ionian",  "dorian",  "phrygian", "lydian",  "mixolydian",
+    "aeolian", "locrian", "harmonic", "melodic",
 };
 
 /** A Scale is defined as an array of 7 unsigned integers */
@@ -69,31 +54,25 @@ typedef std::array<int, 7> Scale;
 /** Map of mode names to scale, represented as an array of 7 semitone
  * differences from the root note of the scale */
 const std::map<std::string, Scale> modes = {
-    //// Diatonic heptatonic scales ////
-    // Major (Ionian): "ionian" or "major"
-    {"ionian", {0, 2, 4, 5, 7, 9, 11}},     // W–W–H–W–W–W–H
-    {"dorian", {0, 2, 3, 5, 7, 9, 10}},     // W–H–W–W–W–H–W
-    {"phrygian", {0, 1, 3, 5, 7, 8, 10}},   // H–W–W–W–H–W–W
-    {"lydian", {0, 2, 4, 6, 7, 9, 11}},     // W–W–W–H–W–W–H
-    {"mixolydian", {0, 2, 4, 5, 7, 9, 10}}, // W–W–H–W–W–H–W
-    // Natural minor (Aeolian): "aeolian" or "minor"
-    {"aeolian", {0, 2, 3, 5, 7, 8, 10}}, // W–H–W–W–H–W–W
-    {"locrian", {0, 1, 3, 5, 6, 8, 10}}, // H–W–W–H–W–W–W
-    //// Non-diatonic heptatonic cales ////
-    // Harmonic minor (Aeolian ♯7): "harmonic"
-    {"harmonic", {0, 2, 3, 5, 7, 8, 11}}, // W–H–W–W–H–Aug2nd–H
-    // Ascending melodic minor aka Jazz minor (Ionian ♭3): "melodic", "jazz"
-    {"melodic", {0, 2, 3, 5, 7, 9, 11}}, // W–H–W–W–W–W–H
+    {"ionian", {0, 2, 4, 5, 7, 9, 11}},      // W–W–H–W–W–W–H
+    {"dorian", {0, 2, 3, 5, 7, 9, 10}},      // W–H–W–W–W–H–W
+    {"phrygian", {0, 1, 3, 5, 7, 8, 10}},    // H–W–W–W–H–W–W
+    {"lydian", {0, 2, 4, 6, 7, 9, 11}},      // W–W–W–H–W–W–H
+    {"mixolydian", {0, 2, 4, 5, 7, 9, 10}},  // W–W–H–W–W–H–W
+    {"aeolian", {0, 2, 3, 5, 7, 8, 10}},     // W–H–W–W–H–W–W
+    {"locrian", {0, 1, 3, 5, 6, 8, 10}},     // H–W–W–H–W–W–W
+    {"harmonic", {0, 2, 3, 5, 7, 8, 11}},    // W–H–W–W–H–Aug2nd–H
+    {"melodic", {0, 2, 3, 5, 7, 9, 11}},     // W–H–W–W–W–W–H
 };
 
 /** Given a root note and mode, return a scale, represented as an array of 7
  * MIDI note numbers */
-Scale getScale(std::string scaleRootNoteName = "C", int octave = 4, std::string mode = "ionian")
-{
+Scale getScale(std::string scaleRootNoteName = "C",
+               int octave = 4,
+               std::string mode = "ionian") {
   const int rootNoteNumber = getMidiNote(scaleRootNoteName, octave);
   Scale scale = modes.find(mode)->second;
-  for (auto i = 0; i <= mode.size(); i++)
-  {
+  for (auto i = 0; i <= mode.size(); i++) {
     scale[i] = scale[i] + rootNoteNumber;
   }
   return scale;
@@ -102,27 +81,27 @@ Scale getScale(std::string scaleRootNoteName = "C", int octave = 4, std::string 
 /** A Chord is a vector of integers */
 typedef std::vector<int> Chord;
 
-/** Map of chord names to chords (represented as a vector of steps in a scale) */
-const std::map<std::string, Chord> chordShapes = {
-    {"triad", {0, 2, 4}}};
+/** Map of chord names to chords (represented as a vector of steps in a scale)
+ */
+const std::map<std::string, Chord> chordShapes = {{"triad", {0, 2, 4}}};
 
 /** Given a root note, mode, and number of notes, return a chord, represented as
  * an array of MIDI note numbers */
-Chord getChord(std::string scaleRootNoteName = "C", int octave = 4, std::string mode = "ionian", int numberOfNotes = 3)
-{
+Chord getChord(std::string scaleRootNoteName = "C",
+               int octave = 4,
+               std::string mode = "ionian",
+               int numberOfNotes = 3) {
   const Scale scale = getScale(scaleRootNoteName, octave, mode);
   const Chord shape = chordShapes.find("triad")->second;
   Chord indexes;
   Chord chord;
 
-  for (auto step = 0; step < numberOfNotes; step++)
-  {
+  for (auto step = 0; step < numberOfNotes; step++) {
     int note = shape[step % shape.size()] + 7 * floor(step / shape.size());
     indexes.push_back(note);
   }
 
-  for (const auto &note : indexes)
-  {
+  for (const auto& note : indexes) {
     const auto octaveOffset = floor(note / 7);
     chord.push_back(scale[((note % 7) + 7) % 7] + 12 * octaveOffset);
   }
@@ -130,4 +109,4 @@ Chord getChord(std::string scaleRootNoteName = "C", int octave = 4, std::string 
   return chord;
 }
 
-#endif // MIDI_TOOLS_H
+#endif  // MIDI_TOOLS_H
--- midi.cpp
+++ midi.cpp
@@ -1,14 +1,15 @@
-#include "midi-tools.h"
 #include <iostream>
+
+#include "midi-tools.h"
 
 int main() {
   const Scale scale = getScale("A", 4, "aeolian");
-  for (const int &value : scale) {
+  for (const int& value : scale) {
     std::cout << value << ' ';
   }
   std::cout << "\n";
   const Chord chord = getChord("A", 4, "aeolian", 3);
-  for (const int &value : chord) {
+  for (const int& value : chord) {
     std::cout << value << ' ';
   }
 }
