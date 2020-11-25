6bbd2899fc1de3bb9f9601a95f1647c6003071e1
Nathan Ward <nathan.ward@puppet.com>
2020-11-24 23:50:43 -0800

    TEMP

--- index.js
+++ index.js
@@ -53,19 +53,6 @@ export function getMidiNote(noteName = 'C', octave = 4) {
   );
 }
 
-/** Array of mode names */
-export const modeNames = [
-  'ionian',
-  'dorian',
-  'phrygian',
-  'lydian',
-  'mixolydian',
-  'aeolian',
-  'locrian',
-  'harmonic',
-  'melodic',
-];
-
 /** Map of mode names to scale, represented as an array of 7 semitone
  * differences from the root note of the scale */
 export const modes = {
@@ -87,35 +74,64 @@ export const modes = {
   melodic: [0, 2, 3, 5, 7, 9, 11], // W–H–W–W–W–W–H
 };
 
-/** Given a root note and mode, return a scale, represented as an array of 7
- * MIDI note numbers */
-export function getScale(scaleRootNoteName = 'C', octave = 4, mode = 'ionian') {
-  const rootNoteNumber = getMidiNote(scaleRootNoteName, octave);
+/** Given the tonic note name and mode, return a scale, represented as an array
+ * of 7 MIDI note numbers (the lowest consecutive sequence ) */
+export function getScale(pitchClass = 'A', modeName = 'aeolian') {
+  const tonic = getMidiNote(pitchClass, -1);
 
-  return modes[mode].map((interval) => rootNoteNumber + interval);
+  return modes[modeName].map((interval) => tonic + interval);
 }
 
-/** Given a root note, mode, and number of notes, return a chord, represented as
- * an array of MIDI note numbers */
+/** Given a root note MIDI number, scale tonic note name, scale mode, and number of notes, return a chord, represented as an array of MIDI note numbers */
 export function getChord(
-  scaleRootNoteName = 'C',
-  octave = 4,
-  mode = 'ionian',
-  numberOfNotes = 3,
+  rootNote = 69,
+  pitchClass = 'A',
+  modeName = 'aeolian',
+  chordShapeName = 'triad',
+  /* `numberOfNotes` defaults to the number of notes in the chord shape, but can be decreased or increased to get more notes in higher octaves */
+  numberOfNotesParam,
 ) {
-  const scale = getScale(scaleRootNoteName, octave, mode);
-  const shapes = { triad: [0, 2, 4] };
-  const shape = shapes.triad;
-  const indexes = [];
+  const scale = getScale(pitchClass, modeName);
+  // `chordTemplates` are represented as in-scale intervals, not semitones, and
+  // are 0-indexed, so a third is 2 and a fifth is 4 for example.
+  const chordTemplates = {
+    triad: [0, 2, 4],
+    power: [0, 4, 7],
+    seventh: [0, 2, 4, 6],
+    ninth: [0, 2, 4, 6, 8],
+  };
+  const chordTemplate = chordTemplates[chordShapeName];
+  const numberOfNotes = numberOfNotesParam
+    ? numberOfNotesParam
+    : chordTemplate.length;
+  let chordShape = [];
 
-  for (let step = 0; step < numberOfNotes; step++) {
-    indexes.push(
-      shape[step % shape.length] + 7 * Math.floor(step / shape.length),
-    );
+  if (numberOfNotes === chordTemplate.length) {
+    chordShape = chordTemplate;
+  } else {
+    // `numberOfNotes` can be less than or greater than number of notes in `chordTemplate`, in which case extra notes in higher octave are used
+    for (let i = 0; i < numberOfNotes; i++) {
+      // Offset is 0 when `numberOfNotes` <= number of notes in `chordTemplate`
+      const octaveOffset = Math.floor(i / chordTemplate.length);
+      const note = chordTemplate[i % chordTemplate.length] + 7 * octaveOffset;
+      chordShape.push(note);
+    }
   }
 
-  return indexes.map((note) => {
-    const octaveOffset = Math.floor(note / 7);
-    return scale[((note % 7) + 7) % 7] + 12 * octaveOffset;
+  const scaleOctaveOffset = Math.floor((rootNote - scale[0]) / 12);
+  const shiftedScale = scale.map((note) => note + 12 * scaleOctaveOffset);
+  const rootNoteIndexInShiftedScale = shiftedScale.indexOf(rootNote);
+  const isRootNoteInKey = rootNoteIndexInShiftedScale !== -1;
+  if (!isRootNoteInKey) return [];
+
+  const chord = chordShape.map((chordShapeNote) => {
+    const octaveOffset = Math.floor(
+      (chordShapeNote + rootNoteIndexInShiftedScale) / 7,
+    );
+    const index =
+      (((chordShapeNote + rootNoteIndexInShiftedScale) % 7) + 7) % 7;
+    return shiftedScale[index] + 12 * octaveOffset;
   });
+
+  return chord;
 }
--- midi-test.js
+++ midi-test.js
@@ -1,7 +1,10 @@
-import { getScale, getChord } from './index.js';
+import { getMidiNote, getScale, getChord } from './index.js';
 
-const scale = getScale("A", 4, "aeolian");
+const scale = getScale("A", "aeolian");
 console.log('The A minor scale:', scale.join(' '));
 
-const chord = getChord("A", 4, "aeolian", 3);
-console.log('The A minor chord:', chord.join(' '));
+const root = getMidiNote('A', 4);
+let chord = getChord(root, 'A', "aeolian");
+console.log('The A minor triad:', chord.join(' '));
+chord = getChord(root, 'A', "aeolian", 'ninth');
+console.log('The A minor ninth chord:', chord.join(' '));
